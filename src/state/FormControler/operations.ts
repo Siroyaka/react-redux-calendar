a5c6ed3334cc203c7e7fa5bc4cfc81284529bbcc
Siroyaka <29934865+Siroyaka@users.noreply.github.com>
2020-05-01 14:02:49 +0900

    add formcontroler

---
+++ src/state/FormControler/operations.ts
@@ -0,0 +1,19 @@
+import actions from './actions';
+
+const switchLeftDrawerVisible = actions.switchLeftDrawerVisible;
+
+const openScheduleRegister = actions.openScheduleRegister;
+
+const closeScheduleRegister = actions.closeScheduleRegister;
+
+const openScheduleViewer = actions.openScheduleViewer;
+
+const closeScheduleViewer = actions.closeScheduleViewer;
+
+export default {
+    switchLeftDrawerVisible,
+    openScheduleRegister,
+    closeScheduleRegister,
+    openScheduleViewer,
+    closeScheduleViewer,
+}
